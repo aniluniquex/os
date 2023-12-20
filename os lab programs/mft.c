b11dde415a941f7e358e795460cfcddf17d00944
aniluniquex <103011235+aniluniquex@users.noreply.github.com>
2023-12-20 07:55:30 +0530

    Add files via upload

---
+++ os lab programs/mft.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ms,i,ps[20],n,size,p[20],s,intr=0;
+printf("Enter size of memory:");
+scanf("%d",&ms);
+printf("Enter memory for OS:");
+scanf("%d",&s);
+ms-=s;
+printf("Enter no.of partitions to be divided:");
+scanf("%d",&n);
+size=ms/n;
+for(i=0;i<n;i++)
+{
+printf("Enter process and process size");
+scanf("%d%d",&p[i],&ps[i]);
+if(ps[i]<=size)
+{
+intr=intr+size-ps[i];
+printf("process%d is allocated\n",p[i]);
+}
+else
+printf("process%d is blocked",p[i]);
+}
+printf("internal fragmentation is %d",intr);
+getch();
+}
